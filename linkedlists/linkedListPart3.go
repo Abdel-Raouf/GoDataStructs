afb6788137a6370eb4675a2213f09dbb8e3e60d5
Kyle Rose <roseky@onid.oregonstate.edu>
2016-03-20 04:22:36 -0700

    add reverse

---
+++ linkedlists/linkedListPart3.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"strconv"
+)
+
+type student struct {
+	name string
+	age int
+	ssn string
+	next * student
+}
+
+func reverse(curr * student) *student {
+  if (curr.next == nil){
+    return curr
+  }else{
+    newHead := reverse(curr.next)
+    curr.next.next = curr
+    curr.next = nil
+    return newHead
+  }
+}
+
+func main() {
+	students := new(student)
+	students.next = nil
+
+  if len(os.Args) < 2{
+    fmt.Println("There is no file")
+    return
+  }
+
+	filename := os.Args[1]
+	data, _ := ioutil.ReadFile(filename)
+
+	for _, line := range strings.Split(string(data), "\n") {
+
+		if line == "" {
+			continue
+		}
+		td := strings.Split(string(line), " ")
+
+		ts := &student{
+      name: td[0],
+      ssn: td[2],
+      next: students.next,
+    }
+    ts.age, _ = strconv.Atoi(td[1])
+
+		students.next = ts
+	}
+
+	fmt.Println("Original----")
+  for s := students.next; s != nil; s = s.next {
+		fmt.Println(s.name, s.age, s.ssn)
+	}
+
+  students = reverse(students)
+
+  for s := students; s.next != nil; s = s.next {
+    fmt.Println(s.name, s.age, s.ssn)
+  }
+}
